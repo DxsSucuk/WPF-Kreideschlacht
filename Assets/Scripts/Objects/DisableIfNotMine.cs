f698c7794c3ef6d5b77c228a28da616d8392461a
Presti <dxssucuk@hotmail.com>
2024-01-12 08:06:32 +0100

    Bug fix.

---
+++ Assets/Scripts/Objects/DisableIfNotMine.cs
@@ -0,0 +1,12 @@
+﻿using Photon.Pun;
+
+public class DisableIfNotMine : MonoBehaviourPun
+{
+    private void Awake()
+    {
+        if (!photonView.IsMine)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
